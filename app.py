fe21d3545be3a34faf2b273f7a415e2321982ab0
Linzhe Cai <cailinzhe@gmail.com>
2024-11-14 06:58:27 +0000

    init

---
+++ app.py
@@ -0,0 +1,28 @@
+"""
+A sample Hello World server.
+"""
+import os, datetime
+
+from flask import Flask, render_template
+
+# pylint: disable=C0103
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    message = "It's running!"
+
+    """Get Cloud Run environment variables."""
+    dummy_times = [
+        datetime.datetime(2018, 1, 1, 10, 0, 0),
+        datetime.datetime(2018, 1, 2, 10, 30, 0),
+        datetime.datetime(2018, 1, 3, 11, 0, 0),
+    ]
+
+    return render_template("index.html", times=dummy_times)
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
